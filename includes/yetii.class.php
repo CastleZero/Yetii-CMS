4d5803e29545aa94dafda33ae3a4f30c460d4a46
Joseph Duffy <JosephDuffy@me.com>
2013-01-22 18:48:57 +0000

    Updated version number

--- includes/yetii.class.php
+++ includes/yetii.class.php
@@ -2,7 +2,7 @@
 
 class Yetii {
 	private $name, $channel, $template, $language, $update = false;
-	const VERSION = '0.2.6';
+	const VERSION = '0.2.7';
 
 	public function loadSettings() {
 		$mapper = new Mapper();
