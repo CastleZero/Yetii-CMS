fe9ca4210b55ec4a9fda02a421c4c75fe3732cb1
Joseph Duffy <JosephDuffy@me.com>
2013-01-24 14:49:14 +0000

    Changes to .htaccess to better accommodate directories and files with the same name. Stops /dir and /dir/ serving the same content. /dir now redirects to /dir/

--- includes/yetii.class.php
+++ includes/yetii.class.php
@@ -2,7 +2,7 @@
 
 class Yetii {
 	private $name, $channel, $template, $language, $update = false;
-	const VERSION = '0.2.8';
+	const VERSION = '0.2.9';
 
 	public function loadSettings() {
 		$mapper = new Mapper();
